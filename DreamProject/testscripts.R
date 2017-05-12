a0b2ac667ce6d185db3b29a879cd792634cf8ba1
im281 <iman.mohtashemi@gmail.com>
2017-05-11 17:29:48 -0700

    updating R

---
+++ DreamProject/testscripts.R
@@ -0,0 +1,11 @@
+#Updating R
+
+# installing/loading the package:
+if(!require(installr)) {
+  install.packages("installr"); 
+  require(installr)
+} #load / install+load installr
+
+# using the package:
+updateR()
+
